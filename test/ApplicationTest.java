832b64f1cb5e3e419d503783a9b91f47b0699574
tedwon <iamtedwon@gmail.com>
2015-03-09 13:14:28 +0900

    Fix for compile error

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.codehaus.jackson.JsonNode;
+//import org.codehaus.jackson.JsonNode;
 import org.junit.*;
 
 import play.mvc.*;
